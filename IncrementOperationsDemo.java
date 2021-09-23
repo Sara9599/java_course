e3913d565a437323a2ce3e7c259b8aea4be63009
Sara9599 <61079445+Sara9599@users.noreply.github.com>
2021-09-23 07:50:28 +0530

    Add files via upload

---
+++ IncrementOperationsDemo.java
@@ -0,0 +1,17 @@
+class IncrementOperationsDemo{
+public static void main(String[] args){
+	System.out.println("preincrement :");
+	int a =1;
+	int b=++a;
+	System.out.println("a:"+a);
+	System.out.println("b:"+b);
+
+	System.out.println("postincrement :");
+	int i =100;
+	int j=i++;
+	System.out.println("i:"+i);
+	System.out.println("j:"+j);
+}
+}
+
+	
