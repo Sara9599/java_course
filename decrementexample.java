ed25273ff0c4fb9af469cd1356ffa7f8ec9b780e
Sara9599 <61079445+Sara9599@users.noreply.github.com>
2021-09-23 08:06:55 +0530

    Add files via upload

---
+++ decrementexample.java
@@ -0,0 +1,19 @@
+class decrementexample()
+{
+public static void main(String[] args)
+{
+int a,x;
+a=10;
+x=--a;
+System.out.println("a: "+a);
+System.out.println("x: "+x);
+
+int a,x;
+a=10;
+x=a--;
+System.out.println("a: "+a);
+System.out.println("x: "+x);
+}
+}
+
+
