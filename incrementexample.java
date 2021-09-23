89b2f91ec346ec7cd34a6b3bdbd7bd8e4e48c866
Sara9599 <61079445+Sara9599@users.noreply.github.com>
2021-09-23 08:02:30 +0530

    Add files via upload

---
+++ incrementexample.java
@@ -0,0 +1,11 @@
+class incrementexample(){
+public static void main(String[] args)
+{
+int number=50;
+System.out.println("number is"+number);
+number++;
+System.out.println("numberis:"+number);
+number--;
+System.out.println("number is:"+number);
+}
+}
