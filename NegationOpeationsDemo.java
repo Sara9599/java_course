64c9382f56a3c5c22da7f2b40c5830c208642fa0
Sara9599 <61079445+Sara9599@users.noreply.github.com>
2021-09-23 07:50:45 +0530

    Add files via upload

---
+++ NegationOpeationsDemo.java
@@ -0,0 +1,12 @@
+class NegationOperationsDemo{
+public static void main(String[] args){
+System.out.println("predecrement :");
+
+boolean b=true;
+boolean result=!b;
+System.out.println("b:"+b);
+System.out.println("result:"+result);
+}
+}
+
+
