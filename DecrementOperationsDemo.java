bc5030443cef0841c5e1269584319e685c9afc00
Sara9599 <61079445+Sara9599@users.noreply.github.com>
2021-09-23 07:49:35 +0530

    Add files via upload

---
+++ DecrementOperationsDemo.java
@@ -0,0 +1,15 @@
+class DecrementOperationsDemo{
+public static void main(String[] args){
+System.out.println("predecrement :");
+
+int m=10;
+int n=--m;
+System.out.println("m:"+m);
+System.out.println("n :"+n);
+
+int i=50;
+int j=i--;
+System.out.println("i:"+i);
+System.out.println("j :"+j);
+}
+}
