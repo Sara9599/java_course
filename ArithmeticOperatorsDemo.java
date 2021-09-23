dea30ec719a67b5a44828ebf540c680ef4500802
Sara9599 <61079445+Sara9599@users.noreply.github.com>
2021-09-23 07:49:08 +0530

    Add files via upload

---
+++ ArithmeticOperatorsDemo.java
@@ -0,0 +1,33 @@
+class ArithmeticOperatorsDemo{
+public static void main(String[] args)
+{
+int a=10;
+int b=20;
+int addResult=a+b;
+System.out.println("addResult:"+addResult);
+
+int x=100;
+int y=50;
+int subResult=x-y;
+System.out.println("subResult:"+subResult);
+
+int p=10;
+int q=20;
+int mulResult=p*q;
+System.out.println("mulResult:"+mulResult);
+
+int m=10;
+int n=5;
+int divResult=m/n;
+System.out.println("divResult:"+divResult);
+
+int w=9;
+int d=2;
+int modResult=w%d;
+System.out.println("modResult:"+modResult);
+
+
+
+
+}
+}
