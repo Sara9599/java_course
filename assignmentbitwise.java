930d4d46a3988cc2c71c64839a9eb9e17b58bacd
Anjum Taj <61079445+Sara9599@users.noreply.github.com>
2021-09-24 19:38:34 +0530

    Add files via upload

---
+++ assignmentbitwise.java
@@ -0,0 +1,14 @@
+class assignmentbitwise{
+public static void main(String[] args)
+{
+
+int a = 12;
+int b = 25;
+System.out.println(+a&b);
+
+System.out.println( +a|b);
+
+System.out.println(+a^b);
+}
+}
+
