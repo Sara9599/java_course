fb08cfce9078fc227ae0873f3091a55989398725
Sara9599 <61079445+Sara9599@users.noreply.github.com>
2021-09-24 08:48:45 +0530

    Add files via upload

---
+++ assignmentopearator.java
@@ -0,0 +1,15 @@
+class assignment{
+public static void main(String[] args)
+{
+int m=50;
+int n =10;
+
+n+=m;
+System.out.println(" n is :"+n);
+int i=50;
+int j=10;
+i/=j;
+System.out.println(" i is :"+i);
+
+}
+}
