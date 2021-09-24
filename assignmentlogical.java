42d559ae8d1d951d97010345e158a19309e025d9
Anjum Taj <61079445+Sara9599@users.noreply.github.com>
2021-09-24 20:08:11 +0530

    Add files via upload

---
+++ assignmentlogical.java
@@ -0,0 +1,10 @@
+class assignmentlogical{
+ public static void main(String[] args)
+    {
+       
+        int a = 10, b = 20, c = 20, d = 0;
+        System.out.println((a < b) && (b == c));
+        System.out.println(a > b || c == d);
+	
+    }
+}
