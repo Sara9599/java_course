15318f9a14d42944c767a33270afdfbca372766d
Anjum Taj <61079445+Sara9599@users.noreply.github.com>
2021-09-27 08:22:30 +0530

    Add files via upload

---
+++ AssignmentRelational.java
@@ -0,0 +1,14 @@
+class AssignmentRelational {
+
+   public static void main(String[] args) {
+      int a = 100;
+      int b = 250;
+
+      System.out.println("a == b = " + (a == b) );
+      System.out.println("a != b = " + (a != b) );
+      System.out.println("a > b = " + (a > b) );
+      System.out.println("a < b = " + (a < b) );
+      System.out.println("b >= a = " + (b >= a) );
+      System.out.println("b <= a = " + (b <= a) );
+   }
+}
