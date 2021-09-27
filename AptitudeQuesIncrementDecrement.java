f51768725aa3212e915757a433d6042d0e72047c
Anjum Taj <61079445+Sara9599@users.noreply.github.com>
2021-09-27 08:12:06 +0530

    Add files via upload

---
+++ AptitudeQuesIncrementDecrement.java
@@ -0,0 +1,34 @@
+class AptitudeQuesIncrementDecrement{
+
+    public static void main(String[] args) 
+    {
+        int i = 11;
+        i = i++ + ++i;
+        System.out.println(i);
+
+	int a=11, b=22, c;
+        c = a + b + a++ + b++ + ++a + ++b;
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+        System.out.println("c="+c);
+
+	
+ 	int x=0;
+        x = x++ - --x+ ++x - x--;
+        System.out.println(i);
+
+
+	int l=1, j=2, k=3;
+        int m = i-- - j-- - k--;
+        System.out.println("l="+l);
+        System.out.println("j="+j);
+        System.out.println("k="+k);
+        System.out.println("m="+m);
+
+
+	int y= 0, z = 0;
+        int p = --y * --z * z-- * y--;
+        System.out.println(p);
+
+    }
+}
