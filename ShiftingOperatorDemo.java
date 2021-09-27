36a8a10b9945818cb5edb6b1d82038bb73ae3b8b
Anjum Taj <61079445+Sara9599@users.noreply.github.com>
2021-09-27 08:57:34 +0530

    Add files via upload

---
+++ ShiftingOperatorDemo.java
@@ -0,0 +1,11 @@
+class ShiftingOperatorDemo   
+{  
+public static void main(String args[])  
+{  
+byte x, y;  
+x=20;  
+y=10;  
+System.out.println("Bitwise Left Shift: x<<2 = "+(x<<2));  
+System.out.println("Bitwise Right Shift: y>>2 = "+(y>>2));
+}
+}  
