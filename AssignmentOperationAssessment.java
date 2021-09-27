0ff202ec4cda017af4b2eafd91c73904c138bdda
Anjum Taj <61079445+Sara9599@users.noreply.github.com>
2021-09-27 08:22:01 +0530

    Add files via upload

---
+++ AssignmentOperationAssessment.java
@@ -0,0 +1,11 @@
+class AssignmentOperationAssessment{
+public static void main(String[] args)
+    {
+  int n= 10, m = 20;
+  
+  System.out.println("n = " + n);
+  System.out.println("m = " + m);
+n += m;
+System.out.println("n = " + n);
+}
+}
