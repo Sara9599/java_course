de0776a2a0e3ad81f5d6064acea1a56d170be931
Anjum Taj <61079445+Sara9599@users.noreply.github.com>
2021-10-28 09:48:24 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,15 @@
+class EvenOdd{
+public static void main(String[] args){
+       int num=4;
+       
+       
+       if(num%2==0)
+       {
+        System.out.println("THIS IS EVEN NUMBER");
+       }
+       else
+       {
+        System.out.println("THIS IS ODD NUMBER");
+       }
+}
+}
