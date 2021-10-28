f2d3c97511dde7010d7ae0d5f7bea4f9354cc2dc
Anjum Taj <61079445+Sara9599@users.noreply.github.com>
2021-10-28 09:47:31 +0530

    Add files via upload

---
+++ DigitDemo2.java
@@ -0,0 +1,16 @@
+class DigitDemo2{
+public static void main(String[] asrgs){
+
+
+    int num=9964;
+    if(num<10000 && num>=1000)
+    {
+      System.out.println("it is a 4 digit number");
+    }
+
+    else
+    {
+      System.out.println("it is not a 4 digit number");  
+    }
+}
+}
