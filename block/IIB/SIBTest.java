a4c6cd4f56f5a7f55272cee20f2ddbe7b91d65ad
Anjum Taj <61079445+Sara9599@users.noreply.github.com>
2021-10-28 18:53:57 +0530

    Add files via upload

---
+++ block/IIB/SIBTest.java
@@ -0,0 +1,9 @@
+package com.xworkz.xworkz_java.block.IIB;
+
+public class SIBTest {
+public static void main(String[] args)
+{
+	StudentSIB student=new StudentSIB();
+	System.out.println(student.name);		
+}
+}
