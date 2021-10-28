937f4601d1efb5a4d151172e494dff0242cb269b
Anjum Taj <61079445+Sara9599@users.noreply.github.com>
2021-10-28 18:26:00 +0530

    Add files via upload

---
+++ block/IIB/Bird.java
@@ -0,0 +1,19 @@
+package com.xworkz.xworkz_java.block.IIB;
+
+public class Bird {
+ static String name;
+ static String color;
+ static int age;
+ {
+	 name="sparrow";
+	 color="white";
+	 age=2;
+	 System.out.println("IIB called");
+ }
+ static{
+	 name="parrot";
+	 color="green";
+	 age=2;
+	 System.out.println("SIB called"); 
+ }
+}
---
+++ block/IIB/IIBTest.java
@@ -0,0 +1,11 @@
+package com.xworkz.xworkz_java.block.IIB;
+
+public class IIBTest {
+public static void main(String[] args)
+{
+	Bird bird=new Bird();
+	System.out.println(bird.name);
+	 System.out.println(bird.color);
+	 System.out.println(bird.age);
+}
+}
