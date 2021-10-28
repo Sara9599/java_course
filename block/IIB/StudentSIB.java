580f19a2f53324f9548297e36bd7b438640c34c2
Anjum Taj <61079445+Sara9599@users.noreply.github.com>
2021-10-28 18:54:25 +0530

    Add files via upload

---
+++ block/IIB/StudentSIB.java
@@ -0,0 +1,17 @@
+package com.xworkz.xworkz_java.block.IIB;
+
+public class StudentSIB {
+
+	static String name;
+	int rollno;
+	int std;
+	char section;
+	static {
+		name="John";
+		
+		System.out.println("SIB called");	
+	}
+	
+	
+}
+
