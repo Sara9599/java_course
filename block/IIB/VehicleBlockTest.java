a4c95b8a83f16fe8f41a9a6457ee6fb3fe064247
Anjum Taj <61079445+Sara9599@users.noreply.github.com>
2021-10-28 18:54:41 +0530

    Add files via upload

---
+++ block/IIB/VehicleBlockTest.java
@@ -0,0 +1,19 @@
+package com.xworkz.xworkz_java.block.IIB;
+
+public class VehicleBlockTest {
+public static void main(String[] args)
+{
+	String color;
+	int price;
+	String name;
+	{
+	color="black";
+	price=200000;
+	name="Bullet";
+	System.out.println("IIB called");
+	System.out.println(color);
+	System.out.println(name);
+	
+	}
+}
+}
