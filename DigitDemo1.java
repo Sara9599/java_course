7da9cfa1f1a4ef33190bc047a03cf8e2693c4078
Anjum Taj <61079445+Sara9599@users.noreply.github.com>
2021-10-28 09:47:16 +0530

    Add files via upload

---
+++ DigitDemo1.java
@@ -0,0 +1,16 @@
+class DigitDemo1{
+public static void main(String[] asrgs){
+
+
+    int num=2;
+    if(num <100 && num>=10)
+    {
+       System.out.println("its a 2 digit number");  
+    }
+    else
+    {  
+        System.out.println("its not a 2 digit number");  
+    }
+      
+}
+}
